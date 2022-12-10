dda0327481db6a2f2d24cae946727e69161f237c
Sagir Hossain <sagir@impelitsolutions.com>
2022-12-10 14:33:01 +0600

    application dockerized

--- database/migrations/1670650907394_users.ts
+++ database/migrations/1670650907394_users.ts
@@ -6,10 +6,10 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('firstname', 255).notNullable()
+      table.string('lastname', 255).notNullable()
       table.string('email', 255).notNullable().unique()
-      table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
-
+      table.string('password', 180)
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
