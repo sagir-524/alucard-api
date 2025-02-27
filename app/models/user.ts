eed818da88abc8a9d827d73481a2941bdf0f42c0
Sagir Hossain <sagir@impelitsolutions.com>
2025-02-27 23:58:52 +0600

    Initial commit

---
+++ app/models/user.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare firstName: string
+
+  @column()
+  declare lastName: string
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare isAdmin: boolean
+
+  @column()
+  declare isSuperAdmin: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @column.dateTime({ autoCreate: false, autoUpdate: false })
+  declare deactivatedAt: DateTime | null
+}
