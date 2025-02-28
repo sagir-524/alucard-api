64eb7ae5800095a05df71fd897fcbc0a62b5d64b
Sagir Hossain <sagir@impelitsolutions.com>
2025-02-28 23:26:36 +0600

    linted

--- start/kernel.ts
+++ start/kernel.ts
@@ -43,5 +43,5 @@ router.use([
  * the routes or the routes group.
  */
 export const middleware = router.named({
-  auth: () => import('#middleware/auth_middleware')
+  auth: () => import('#middleware/auth_middleware'),
 })
