e6d1f627bc84d938cc9af648f5974ea95d793053
Sagir Hossain <sagir@impelitsolutions.com>
2025-03-08 03:26:36 +0600

    Me api endpoind added

--- start/routes/auth_routes.ts
+++ start/routes/auth_routes.ts
@@ -1,9 +1,11 @@
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 router
   .group(() => {
     router.post('login', '#controllers/auth_controller.login').as('login')
     router.post('refresh', '#controllers/auth_controller.refresh').as('refresh')
+    router.get('me', '#controllers/auth_controller.me').use(middleware.auth()).as('me')
   })
   .prefix('api/v1')
   .as('api.v1')
